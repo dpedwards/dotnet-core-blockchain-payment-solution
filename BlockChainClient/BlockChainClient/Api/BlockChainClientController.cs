1c7591733e91ba4d040c8be7a7ae1e9ac9f96fd0
Davain Pablo Edwards <core8@gmx.net>
2019-03-01 15:20:25 +0100

    :star: add action method to generate private and public key for wallet

---
+++ BlockChainClient/BlockChainClient/Api/BlockChainClientController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlockChainClient.Api
+{
+
+   /**
+    * Used to define blockchain client interactions
+    * 
+    * @author Davain Pablo Edwards
+    * @license MIT 
+    * @version 1.0
+    */
+    [Produces("application/json")]
+    [Route("api/BlockChainClient")]
+    [ApiController]
+    public class BlockChainClientController : ControllerBase
+    {
+
+        /*
+         * NewWallet() action Method to generate private and public key for wallet 
+         * 
+         * @return response
+         */ 
+        [HttpGet("wallet/new")]
+        public IActionResult NewWallet()
+        {
+            var wallet = RSA.RSA.KeyGenerate();
+            var response = new
+            {
+                privateKey = wallet.PrivateKey,
+                publicKey = wallet.PublicKey
+            };
+            return Ok(response);
+        }
+
+    }
+}
